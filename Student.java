66ff63889607f9fcf835a5676f17a9bdb82bd310
Sanjay-082004 <sanjaysampathviji2004@gmail.com>
2025-08-06 05:59:22 +0530

    My first commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student {
+  public static void main(String[] args){
+  String name = "sanjay";
+  long phn = 6369606927L;
+  int totalmark = 450;
+  float percentage = 10.0f;
+  char grade = 'A' ;
+ System.out.println("name:"+name) ;
+ System.out.println("phn:"+phn) ;
+ System.out.println("totalmark:"+totalmark) ;
+ System.out.println("percentage:"+percentage) ;
+ System.out.println("grade:"+grade) ;
+}
+}
