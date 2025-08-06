6d1499fb8e12a8c98c4fee474b093600b9e82942
Sanjay-082004 <sanjaysampathviji2004@gmail.com>
2025-08-06 17:32:35 +0530

    MY FIRST COMMIT

---
+++ Family.java
@@ -0,0 +1,11 @@
+public class Family{
+ public static void main (String[]args){
+String father = "jeyapalu";
+String mother = "sangetha";
+String brother = "venkat";
+System.out.println(father);
+System.out.println(mother);
+System.out.println(brother);
+}
+}
+
