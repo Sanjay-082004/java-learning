738b51ff460f3b27d40a5b842cdd31bb73ce4889
Sanjay-082004 <sanjaysampathviji2004@gmail.com>
2025-08-12 05:52:51 +0530

    operator

--- Family.java
+++ Family.java
@@ -1,11 +1,8 @@
 public class Family{
  public static void main (String[]args){
-String father = "jeyapalu";
-String mother = "sangetha";
-String brother = "venkat";
-System.out.println(father);
-System.out.println(mother);
-System.out.println(brother);
+     int sum1=10;
+      sum1 += 5;
+     System.out.println(sum1);
 }
 }
 
