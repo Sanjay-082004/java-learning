a61e6ef57d11baa0a2afbe0f20ace17bd606fb6d
Sanjay-082004 <sanjaysampathviji2004@gmail.com>
2025-08-26 04:10:57 +0530

    operator

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345; // Example number
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
